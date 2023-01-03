e3c3a30e21cd1e707722bd1440bc43f9084a6a4b
developneoguri <developneoguri@gmail.com>
2023-01-03 18:58:14 +0900

    [Commit3] 2023.01.03 - next step 및 go back 관련 버튼 이동 관련 함수 적용 - 각 페이지별 선택 요소 버튼들 관련 동작 함수 적용 - Monthly ↔ Yearly 버튼 디자인 추가 (동작 함수 및 버튼 전환 애니메이션은 작업 중)

--- assets/modules/main.js
+++ assets/modules/main.js
@@ -4,10 +4,44 @@ const context = document.querySelector("#context");
 const selectState = {
   nowStep: 1,
   period: "Monthly",
-  step1: "",
-  step2: "",
-  step3: "",
-  step4: "",
+  plan: "arcade",
+  addon: "",
+};
+
+// render event object when user move to step pages
+const renderEvent = {
+  step1: () => {},
+
+  step2: () => {
+    const palnGroup = document.getElementsByClassName("plan");
+    // Array.from 관련 내용 정리 ★
+    Array.from(palnGroup).forEach((element) => {
+      if (selectState.plan === element.id) {
+        element.classList.add("border-purplish-blue", "bg-magnolia");
+      } else {
+        element.classList.remove("border-purplish-blue", "bg-magnolia");
+      }
+    });
+  },
+
+  step3: () => {
+    const checkboxGroup = document.getElementsByClassName("checkbox");
+    // Array.from 관련 내용 정리 ★
+    Array.from(checkboxGroup).forEach((element) => {
+      if (selectState.addon.indexOf(element.id) !== -1) {
+        element.parentNode.classList.add("border-purplish-blue", "bg-magnolia");
+        element.classList.add("bg-purplish-blue", "border-purplish-blue");
+      } else {
+        element.parentNode.classList.remove(
+          "border-purplish-blue",
+          "bg-magnolia"
+        );
+        element.classList.remove("bg-purplish-blue", "border-purplish-blue");
+      }
+    });
+  },
+
+  step4: () => console.log(selectState),
 };
 
 // place first page html element when the webpage is rendering first
@@ -18,7 +52,6 @@ fetch("assets/pages/step1").then((response) => {
 });
 
 // click Event when user clicked monthly and yearly switch button
-
 function switchMonthlyOrYearlyState() {
   if (selectState.period === "Monthly") {
     return selectState.period === "Yearly";
@@ -35,10 +68,11 @@ function moveToNextStepPage() {
     .then((response) => response.text())
     .then((text) => {
       context.innerHTML = text;
+      renderEvent[`step${selectState.nowStep}`]();
     });
 }
 
-//click event when user clicked Go Back button
+// click event when user clicked Go Back button
 function moveToPreviousStepPage() {
   selectState.nowStep = selectState.nowStep - 1;
 
@@ -46,5 +80,42 @@ function moveToPreviousStepPage() {
     .then((response) => response.text())
     .then((text) => {
       context.innerHTML = text;
+      renderEvent[`step${selectState.nowStep}`]();
     });
+}
+
+//step2 - click event when user clicked planning option
+function selectPlanningOption(event) {
+  const planGroup = document.getElementsByClassName("plan");
+  // Array.from 관련 내용 정리 ★
+  Array.from(planGroup).forEach((element) => {
+    if (event.target.id === element.id) {
+      element.classList.add("border-purplish-blue", "bg-magnolia");
+      selectState.plan = element.id;
+    } else {
+      element.classList.remove("border-purplish-blue", "bg-magnolia");
+    }
+  });
+}
+
+//step3 - click event when user clicked add-on option
+function selectAddOnOption(event) {
+  const checkbox = event.target.querySelector(".checkbox");
+
+  if (checkbox.classList.contains("bg-purplish-blue") === true) {
+    checkbox.classList.remove("bg-purplish-blue", "border-purplish-blue");
+    event.target.classList.remove("border-purplish-blue", "bg-magnolia");
+    selectState.addon = selectState.addon.filter(
+      (element) => element !== checkbox.id
+    );
+  } else {
+    checkbox.classList.add("bg-purplish-blue", "border-purplish-blue");
+    event.target.classList.add("border-purplish-blue", "bg-magnolia");
+    selectState.addon = [...selectState.addon, checkbox.id];
+  }
+}
+
+//step4 - render event when user move to page step 4;
+function applyUserSelectData() {
+  console.log(selectState);
 }
